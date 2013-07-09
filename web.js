213ec401b20fcc77d2a5faa2e1685d8281f584a4
John Rickerd <john@rickerd.org>
2013-07-08 21:43:52 -0600

    adding idea project.  modifying app.  adding index.html.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var index = fs.readFileSync('index.html');
+    console.log(index);
+    response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
